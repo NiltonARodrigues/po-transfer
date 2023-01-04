7373d4505092a5b6dd323195e9a8f58d30f3c960
Nilton Rodrigues <nilton.rodrigues@totvs.com.br>
2023-01-04 14:42:24 -0300

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PoModule, PoDynamicModule, PoButtonModule, PoStepperModule, PoListViewModule, PoContainerModule, PoLoadingModule, PoWidgetModule } from '@po-ui/ng-components';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { PoGaugeModule } from '@po-ui/ng-components';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PoModule,
+    HttpClientModule,
+    RouterModule.forRoot([]),
+    PoDynamicModule,
+    PoButtonModule,
+    HttpClientModule,
+    PoStepperModule,
+    PoListViewModule,
+    PoContainerModule,
+    PoLoadingModule ,
+    PoWidgetModule ,
+    PoGaugeModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
